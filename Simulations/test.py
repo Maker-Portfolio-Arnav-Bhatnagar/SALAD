5f2b06849bf23b0d5d310e9f2a695c310aee7b51
arnavbhatnagar50 <arnavbhatnagar50@gmail.com>
2026-07-21 11:11:04 +0000

    Jul 21, 2026, 4:41 PM

--- Simulations/test.py
+++ Simulations/test.py
@@ -2,12 +2,8 @@ from pathlib import Path
 import mujoco
 import mujoco.viewer
 
-# Get the folder containing this script
-script_dir = Path(__file__).parent
-
-# Build the full path to scene.xml
-scene_path = script_dir / "franka_emika_panda" / "scene.xml"
-
+script_dir = Path(__file__).parent # Get the folder containing this script
+scene_path = script_dir / "franka_emika_panda" / "scene.xml" # Build the full path to scene.xml
 print("Loading:", scene_path)
 
 model = mujoco.MjModel.from_xml_path(str(scene_path))
