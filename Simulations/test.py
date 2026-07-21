78b37f1b6ccffe584930997425a387741275a13d
arnavbhatnagar50 <arnavbhatnagar50@gmail.com>
2026-07-21 11:17:52 +0000

    Jul 21, 2026, 4:47 PM

--- Simulations/test.py
+++ Simulations/test.py
@@ -9,11 +9,12 @@ print("Loading:", scene_path)
 model = mujoco.MjModel.from_xml_path(str(scene_path))
 data = mujoco.MjData(model)
 
+print("\nNumber of joints:", model.njnt)
+print("Number of actuators:", model.nu)
+print("Number of bodies:", model.nbody)
+
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
         mujoco.mj_step(model, data)
         viewer.sync()
 
-print("Number of joints:", model.njnt)
-print("Number of actuators:", model.nu)
-print("Number of bodies:", model.nbody)
