24514264f90292c7f9deb7ba2ea38b4ab48c199a
arnavbhatnagar50 <arnavbhatnagar50@gmail.com>
2026-07-21 11:01:31 +0000

    Jul 21, 2026, 4:31 PM

--- Simulations/test.py
+++ Simulations/test.py
@@ -1,3 +1,9 @@
 import mujoco
 
-print(mujoco.__version__)
+model = mujoco.Mjmodel.from_xml_path("franka_emika_panda/scene.xml")
+data = mujoco.MjData(model)
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        mujoco.mj_step(model, data)
+        viewer.sync()
