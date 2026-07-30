054f13c6dcf3f0f5a066f8b37ae67ae22ec1c117
arnavbhatnagar50 <arnavbhatnagar50@gmail.com>
2026-07-30 11:52:37 +0000

    Jul 30, 2026, 5:22 PM

---
+++ Simulations/Franka_pick_place.py
@@ -0,0 +1,8 @@
+#Simulation: Franka Pick & Place
+#Using a simple Damped Least Squares (DLS) Jacobian IK instead of Mink
+ 
+#Importing libraries
+import mujoco
+import mujoco.viewer
+import numpy as np
+from pathlib import Path
