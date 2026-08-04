e074450094411871521596f394a2aecf70bba43a
arnavbhatnagar50 <arnavbhatnagar50@gmail.com>
2026-08-04 16:24:32 +0530

    blah blah

--- Code/banana_detector.py
+++ Code/banana_detector.py
@@ -42,10 +42,13 @@ def main(args=None):
     rclpy.init(args=args)
 
     node = banana_detect()
-    rclpy.spin(node)
-
-    node.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == "__main__":
