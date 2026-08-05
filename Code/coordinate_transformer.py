7452cea310a89c84559510425e3057cb3287191e
arnavbhatnagar50 <arnavbhatnagar50@gmail.com>
2026-08-05 12:38:04 +0000

    Aug 5, 2026, 6:08 PM

---
+++ Code/coordinate_transformer.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def coord_transform():
+
+    #Camera to robot base transformation matrix
+    T_CAM_TO_ROBOT = np.array([[ 0.0124, -0.9864, -0.1640,  0.4471],
+                               [-0.9996, -0.0078, -0.0284, -0.4186],
+                               [ 0.0267,  0.1643, -0.9860,  1.2320],
+                               [ 0.0000,  0.0000,  0.0000,  1.0000]])
